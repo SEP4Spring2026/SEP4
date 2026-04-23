d3e799c6f61d773e29db5b20548973a1c6e9d5a8
Matte <matteosaccucci00@gmail.com>
2026-04-23 15:52:08 +0200

    Fix CO2 driver include dependency issue.
    Decouple co2.c from uart.h ringbuffer include-chain and correct uart.h ringbuffer include path so the driver lint/build include resolution is stable.

--- System/IoT_foundation/lib/Drivers/co2.c
+++ System/IoT_foundation/lib/Drivers/co2.c
@@ -7,8 +7,28 @@
  *  Project: SPE4_API
  **********************************************/
 #include "co2.h"
-#include "uart.h"
 #include <stddef.h>
+
+typedef enum {
+    UART_OK = 0,
+    UART_ERROR_INVALID_BAUD_RATE = -1,
+    UART_ERROR_INIT_FAILED = -2,
+    UART_ERROR_INVALID_ID = -3,
+    UART_NO_DATA_AVAILABLE = -4
+} uart_t;
+
+typedef enum {
+    UART0_ID = 0,
+    UART1_ID = 1,
+    UART2_ID = 2,
+    UART3_ID = 3
+} uart_id_t;
+
+typedef void (*rx_callback_t)(uint8_t byte);
+
+uart_t uart_init(uart_id_t uart_id, uint32_t baud_rate, rx_callback_t rx_callback, uint8_t buffer_size);
+uart_t uart_write_bytes(uart_id_t uart_id, uint8_t *data, uint8_t length);
+uart_t uart_read_byte(uart_id_t uart_id, uint8_t *byte);
 
 #define MHZ19B_BAUD_RATE              9600U
 #define MHZ19B_UART_RX_BUFFER_SIZE    32U
