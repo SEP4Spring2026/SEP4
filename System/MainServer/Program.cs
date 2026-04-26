576e0296eb11cd4caee5d3c48e6f3635afe9d56a
Felipe Figueiredo <felipedafig@gmail.com>
2026-04-26 19:14:24 +0200

    DB is on VPS

--- System/MainServer/Program.cs
+++ System/MainServer/Program.cs
@@ -7,13 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 var connectionString = builder.Configuration.GetConnectionString("Default");
+var mlUrl = builder.Configuration["MlServer:Url"]!;
+var dbHost = System.Text.RegularExpressions.Regex.Match(connectionString ?? "", @"Server=([^;]+)").Groups[1].Value;
+Console.WriteLine($"[startup] DB host: {dbHost}");
+Console.WriteLine($"[startup] ML URL : {mlUrl}");
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 36)))
 );
 
 builder.Services.AddHttpClient<MlClient>(client =>
 {
-    client.BaseAddress = new Uri(builder.Configuration["MlServer:Url"]!);
+    client.BaseAddress = new Uri(mlUrl);
 });
 
 var app = builder.Build();
