40ddb224a69684b4d783a6c4470c50caf337126e
shmoao <soma.nagy23@gmail.com>
2026-05-11 15:21:34 +0200

    Fixed a typo in an import that kept the app from running

--- System/FrontEnd/src/App.jsx
+++ System/FrontEnd/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Sidebar } from "./components/Sidebar.jsx";
 import { Topbar } from "./components/Topbar.jsx";
 import { OverviewCard } from "./components/OverviewCard.jsx";
